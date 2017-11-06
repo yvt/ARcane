2f3bff6a9dcef2d5e409f81cf35726ec6e5b15be
yvt <i@yvt.jp>
2017-11-06 14:51:44 +0900

    Add LogManager (copied from Hyper3D)

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,8 @@
-console.log("hello!");
+import { LogManager } from './utils/logger';
+
+const logManager = new LogManager();
+if (process.env.NODE_ENV !== 'production') {
+    logManager.enableAllTopics();
+}
+
+logManager.getLogger('test').log('hello!');
