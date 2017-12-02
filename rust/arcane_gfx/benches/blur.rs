e3c0197c0a0c15c15e4250d92607f28e4923435e
yvt <i@yvt.jp>
2017-12-03 01:19:27 +0900

    Add a benchmark of spherical_blur_phase based on the real use of it

--- rust/arcane_gfx/benches/blur.rs
+++ rust/arcane_gfx/benches/blur.rs
@@ -89,3 +89,66 @@ fn blur3_64(b: &mut test::Bencher) {
 fn blur3_128(b: &mut test::Bencher) {
     run_single(b, 128, 2)
 }
+
+#[bench]
+fn blur_mip_pyramid(b: &mut test::Bencher) {
+    // Based on the parameters from `context.rs`
+    const LOG_SIZE: usize = 6;
+    const SIZE: usize = 1 << LOG_SIZE;
+
+    static BLUR_KERNEL_SIGMA: f32 = 4.0;
+    static BLUR_KERNEL_RATIO: f32 = 2.0;
+
+    let blur_table: Vec<(f32, usize)> = {
+        let mut last_variance = 0.0;
+        (0..5u8)
+            .map(|i| {
+                let size = SIZE >> i;
+                let sigma = (i as f32 - 5.0).exp2();
+
+                // The amount of blur applied on this stage
+                let res_sigma = (sigma * sigma - last_variance).sqrt();
+                last_variance = sigma * sigma;
+
+                // Upper bound of blur amount that can be applied by a single run of
+                // `spherical_blur_phase(..., {0, 1, 2}, ...)`
+                let sigma_limit = 0.5 / BLUR_KERNEL_RATIO;
+                let num_passes = (res_sigma * res_sigma / (sigma_limit * sigma_limit)).ceil();
+
+                let level_sigma = (res_sigma * res_sigma / num_passes).sqrt() * size as f32 /
+                    BLUR_KERNEL_SIGMA;
+
+                (level_sigma, num_passes as usize)
+            })
+            .collect()
+    };
+
+    let kernel = blur::gaussian_kernel(
+        (BLUR_KERNEL_SIGMA * BLUR_KERNEL_RATIO) as usize,
+        BLUR_KERNEL_SIGMA,
+    );
+
+    let count = SIZE * SIZE;
+    let src = vec![vec![0f32; count]; 6];
+    let mut dst = vec![vec![0f32; count]; 6];
+    b.iter(move || for (i, &(kernel_scale, num_passes)) in
+        blur_table.iter().enumerate()
+    {
+        for _ in 0..num_passes {
+            for pass in 0..3 {
+                blur::spherical_blur_phase(
+                    dst.iter_mut()
+                        .map(Vec::as_mut_slice)
+                        .collect::<Vec<_>>()
+                        .as_mut_slice(),
+                    src.iter().map(Vec::as_slice).collect::<Vec<_>>().as_slice(),
+                    SIZE >> i,
+                    &kernel,
+                    kernel_scale,
+                    pass,
+                    blur::StandardCubeMapTrait,
+                );
+            }
+        }
+    })
+}
